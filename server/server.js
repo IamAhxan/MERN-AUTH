93b6c6442e81d9eb6f53142e3d01124c51380e6c
IamAhxan <mohammadahsan7744@gmail.com>
2025-09-30 18:01:43 +0500

    front end url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 const serverPort = process.env.PORT || 4000;
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173', 'https://mern-auth-client-tau.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://mern-auth-client-tau.vercel.app']
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: allowedOrigins, credentials: true }))
